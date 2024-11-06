80b0fa723dce352b09c97c5fe5fa9ae3b1deccc1
lonely-wolf-howl <electruc0095@gmail.com>
2024-11-06 12:13:19 +0900

    fix.

--- src/index.js
+++ src/index.js
@@ -1,17 +1,5 @@
 const NotificationService = require('./services/notification.service');
-const FcmService = require('./services/fcm.service');
-const FcmBuilderService = require('./services/fcm-builder.service');
-const CouponTemplateEn = require('./templates/coupon-en.template');
-const CouponTemplateKo = require('./templates/coupon-ko.template');
-
-const CouponTemplates = {
-  en: CouponTemplateEn,
-  ko: CouponTemplateKo,
-};
 
 module.exports = {
   NotificationService,
-  FcmService,
-  FcmBuilderService,
-  CouponTemplates,
 };
